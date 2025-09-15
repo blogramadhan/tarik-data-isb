ad47c923c8e9a795be405a195e336a90864b0c72
Kurnia Ramadhan <kramadhan@gmail.com>
2025-09-15 09:05:54 +0700

    update code

--- src/config/configKATALOG.ts
+++ src/config/configKATALOG.ts
@@ -15,9 +15,9 @@ export type JenisData = (typeof jenisDataTypes)[number];
 
 export const configMap: Partial<Record<Daerah, Record<string, { apiKey: string, kode: string }>>> = {
     "D197": { // PROV. KALIMANTAN BARAT
-        "Ecat-PaketEPurchasing": { apiKey: "eeb14303-22d7-4193-8793-60bbfdb468fe", kode: "3330" },
-        "Ecat-KomoditasDetail": { apiKey: "af9a6323-71f0-4ddf-843f-ba7052637b28", kode: "3336" },
-        "Ecat-PenyediaDetail": { apiKey: "93c2af98-02e3-483c-b597-e0a790004e90", kode: "3328" },
-        "Ecat-InstansiSatker": { apiKey: "a20ef982-c41b-49bf-8c07-cc710c465f47", kode: "3329" }
+        "Ecat-PaketEPurchasing": { apiKey: "your-api-key", kode: "your-code" },
+        "Ecat-KomoditasDetail": { apiKey: "your-api-key", kode: "your-code" },
+        "Ecat-PenyediaDetail": { apiKey: "your-api-key", kode: "your-code" },
+        "Ecat-InstansiSatker": { apiKey: "your-api-key", kode: "your-code" }
     }
 };
